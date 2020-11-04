2f9296caddd90f62ddac4c0ba9cebd7119b0fb18
Евгений Потопнин <ddjastis@mail.ru>
2020-11-04 15:38:28 +0300

    Made user controller. Updated Program.cs

---
+++ CodeBlogFitness.BL/Controller/UserController.cs
@@ -0,0 +1,61 @@
+﻿using CodeBlogFitness.BL.Model;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CodeBlogFitness.BL.Controller
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь приложения.
+        /// </summary>
+        public User User { get; }
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>
+        /// <param name="user"></param>
+        public UserController (string userName,
+                               string genderName,
+                               DateTime birthDate,
+                               double weight,
+                               double height)
+        {
+            //todo: Проверка данных
+            var gender = new Gender(genderName);
+            User= new User(userName, gender, birthDate, weight, height);
+        }
+
+        /// <summary>
+        /// Получить данные пользователя.
+        /// </summary>
+        /// <returns> Пользователь приложения. </returns>
+        public UserController()
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("users.dat", FileMode.Open))
+            {
+                if (formatter.Deserialize(fs) is User user)
+                {
+                    User = user;
+                }
+                //todo: что делать, если пользователя не прочитали
+            }
+        }
+        /// <summary>
+        /// Сохранить данные пользователя.
+        /// </summary>
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+            using(var fs=new FileStream("users.dat",FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, User);
+            }
+        }
+        
+    }
+}
--- CodeBlogFitness.CMD/Program.cs
+++ CodeBlogFitness.CMD/Program.cs
@@ -10,6 +10,14 @@ namespace CodeBlogFitness.CMD
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вас приветствует приложение CodeBlogFitness!");
+            Console.WriteLine("Введите имя пользователя");
+
+            var name = Console.ReadLine();
+            Console.WriteLine("Введите пол");
+            var gender = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения");
+            var birthDate = DateTime.Parse(Console.ReadLine()); //toto: Переписать
         }
     }
 }
