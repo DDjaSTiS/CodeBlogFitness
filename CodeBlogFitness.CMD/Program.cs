600972ca1f9b0c306f4a2a7169faca83f53e74e6
Евгений Потопнин <ddjastis@mail.ru>
2020-11-04 20:46:01 +0300

    Made functionality of creating new user and reading existing one

--- CodeBlogFitness.CMD/Program.cs
+++ CodeBlogFitness.CMD/Program.cs
@@ -15,16 +15,54 @@ namespace CodeBlogFitness.CMD
             Console.WriteLine("Введите имя пользователя");
 
             var name = Console.ReadLine();
-            Console.WriteLine("Введите пол");
-            var gender = Console.ReadLine();
-            Console.WriteLine("Введите дату рождения");
-            var birthDate = DateTime.Parse(Console.ReadLine()); //toto: Переписать
-            Console.WriteLine("Введите вес");
-            var weight = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите рост");
-            var height = double.Parse(Console.ReadLine());
-            var userController = new UserController(name, gender, birthDate, weight, height);
-            userController.Save();
+          
+            var userController = new UserController(name);
+            if(userController.IsNewUser)
+            {
+                Console.Write("Введите пол: ");
+                var gender = Console.ReadLine();
+                var birthDate=ParseDateTime();
+                var weight = ParseDouble("вес");
+                var height = ParseDouble("рост");
+                userController.SetNewUserData(gender, birthDate, weight, height);
+            }
+            Console.WriteLine(userController.CurrentUser);
+            Console.ReadLine();
+        }
+
+        private static DateTime ParseDateTime()
+        {
+            DateTime birthDate;
+            while (true)
+            {
+                Console.WriteLine("Введите дату рождения (dd.mm.yyyy): ");
+                if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат даты");
+                }
+            }
+
+            return birthDate;
+        }
+
+        private static double ParseDouble(string name)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Введите {name}: ");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный формат {name}");
+                }
+            }
         }
     }
 }
