3151b926322572d530c3450dd8f50d23b6812de2
Евгений Потопнин <ddjastis@mail.ru>
2020-11-04 15:45:33 +0300

    Update program.cs

--- CodeBlogFitness.CMD/Program.cs
+++ CodeBlogFitness.CMD/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeBlogFitness.BL.Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,12 @@ namespace CodeBlogFitness.CMD
             var gender = Console.ReadLine();
             Console.WriteLine("Введите дату рождения");
             var birthDate = DateTime.Parse(Console.ReadLine()); //toto: Переписать
+            Console.WriteLine("Введите вес");
+            var weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите рост");
+            var height = double.Parse(Console.ReadLine());
+            var userController = new UserController(name, gender, birthDate, weight, height);
+            userController.Save();
         }
     }
 }
